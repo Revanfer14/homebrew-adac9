298935f69fde8629539e15baca5b5af7bc8f5d38
Revan Ferdinand <revancrozz@gmail.com>
2026-09-08 10:22:59 +0700

    Update ezconfig.rb

--- Formula/ezconfig.rb
+++ Formula/ezconfig.rb
@@ -1,8 +1,8 @@
 class Ezconfig < Formula
   desc "Automate multi-developer code signing setup for Xcode projects"
   homepage "https://github.com/Revanfer14/ezconfig"
-  url "https://github.com/Revanfer14/ezconfig/archive/refs/tags/0.9.0.tar.gz"
-  sha256 "087c15649eb147a64abeb370dcd82bb0187f4ae0e2084de473a2dc7930278a8b"
+  url "https://github.com/Revanfer14/ezconfig/archive/refs/tags/0.9.1.tar.gz"
+  sha256 "77e3e6ccb56b173057f5e913bf797623aa7067c7d7861863e0870d8ad022778c"
   license "MIT"
 
   depends_on xcode: ["16.0", :build]
